d235c7db3436547285129d1e74a52ab0ace441c6
115100 <git@three-sigma.org>
2025-03-12 22:04:40 +0000

    scss: use ^2.0

--- make.sh
+++ make.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-SCSS_VERSION="0.1.1"
 JSQUEEZE_VERSION="1.0.5"
 PRISMJS_VERSION="4160c577691b74223f3b5515bb920236b96a87e1"
 VIDEOJS_VERSION="4.10.2"
@@ -10,18 +9,6 @@ mkdir tmp
 cd tmp
 
 ## Libraries
-
-# SCSS
-git clone git://github.com/leafo/scssphp.git
-cd scssphp
-git checkout tags/v${SCSS_VERSION}
-rm .gitignore
-rm package.sh
-rm todo
-rm -r site
-rm -rf .git
-cd ..
-mv scssphp ../lib/SCSS
 
 # Twig
 git clone https://github.com/nicolas-grekas/JSqueeze.git
