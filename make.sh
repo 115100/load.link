4cb3ad46c15255d03815c5f6e704f137f816be56
115100 <git@three-sigma.org>
2025-03-13 18:25:41 +0000

    themes: replace faenzai with delft
    Delft is based on Faenzai and adds more MIME icons.

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PRISMJS_VERSION="76dde18a575831c91491895193f56081ac08b0c5" # v1.30.0
-FAENZA_ICONS_URL="http://slackware.uk/sbosrcarch/by-md5/e/9/e9bd6106d13017ce06d24b586259ae9c/faenza-icon-theme_1.3.zip"
+DELFT_VERSION="dd693fcf7cbd2d9fb689fada278a58f4b3e43a75" # v1.15
 
 mkdir tmp
 cd tmp
@@ -19,22 +19,16 @@ cat themes/prism-okaidia.min.css plugins/{line-numbers/prism-line-numbers.min.cs
 cat components/prism-{core,clike,markup,javascript,bash,c,coffeescript,cpp,csharp,css,css-extras,go,haskell,ini,java,latex,markup-templating,objectivec,php,php-extras,python,ruby,scss,sql,swift,twig}.min.js plugins/{line-numbers/prism-line-numbers.min.js,line-highlight/prism-line-highlight.min.js} > ../${THEME_STATIC_PATH}/prismjs/prism.js
 cd ..
 
-# Faenza icons
-mkdir ${THEME_STATIC_PATH}/faenzaicons
-wget ${FAENZA_ICONS_URL} -O faenza_icons.zip
-md5sum faenza_icons.zip | grep 'e9bd6106d13017ce06d24b586259ae9c' || exit 1
-mkdir faenza_icons
-cd faenza_icons
-unzip ../faenza_icons.zip
-tar xzf Faenza.tar.gz
-cd Faenza/mimetypes/96
+# iconpack-delft
+mkdir ${THEME_STATIC_PATH}/delficons
+git clone https://github.com/madmaxms/iconpack-delft.git
+cd iconpack-delft
+git checkout ${DELFT_VERSION}
+cd Delft/mimetypes/96
+ln -s audio-x-vorbis+ogg.svg audio-vorbis.svg
 mkdir ../icons_tmp
 cp -L * ../icons_tmp/
-cd ../icons_tmp
-rm *.icon
-rename.ul "gnome-mime-" "" *
 cd ..
 mv icons_tmp ../../../
 cd ../../..
-cp -r icons_tmp/*-* ${THEME_STATIC_PATH}/faenzaicons/
-cp icons_tmp/none.png ${THEME_STATIC_PATH}/faenzaicons/
+cp -r icons_tmp/* ${THEME_STATIC_PATH}/delfticons/
